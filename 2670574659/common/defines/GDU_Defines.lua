28180578fd7994b93d572809c8e1a19320308c50
EpicTurtle64 <65987552+EpicTurtle64@users.noreply.github.com>
2021-12-13 10:38:33 -0800

    Add files via upload

---
+++ 2670574659/common/defines/GDU_Defines.lua
@@ -0,0 +1,51 @@
+---Optimization
+	NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 400
+	NDefines.NGame.LAG_DAYS_FOR_PAUSE = 720
+	
+	NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 3
+	NDefines.NGame.AIR_LOG_TIMEOUT_HOURS = 24
+	NDefines.NResistance.GARRISON_LOG_MAX_MONTHS = 0
+	
+	NDefines.NNavy.NAVAL_COMBAT_RESULT_TIMEOUT_YEARS = 2
+	NDefines.NNavy.CONVOY_LOSS_HISTORY_TIMEOUT_MONTHS = 24	
+	
+	NDefines.NCountry.EVENT_PROCESS_OFFSET = 25
+	NDefines.NGame.GAME_SPEED_SECONDS = { 0.3, 0.15, 0.1, 0.05, 0.0 }
+	
+	NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 10
+	NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 200
+	NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0	
+
+	NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0
+	NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0
+	NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0
+	NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0
+	NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0 
+	NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0
+	
+---Land Combat Changes
+	NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 10000000
+	NDefines.NMilitary.TRAINING_ATTRITION = 0.00
+	
+	NDefines.NSupply.LOCAL_SUPPLY_PER_AIR_MISSION = 0.2
+	
+	NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 100
+	NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 0.2	
+	
+	NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.10
+	
+---Naval Combat Changes
+	NDefines.NNavy.TRAINING_ACCIDENT_CHANCES = 0
+	
+	NDefines.NNavy.NAVAL_MINES_IN_REGION_MAX = 1
+	NDefines.NNavy.NAVAL_MINES_PLANTING_SPEED_MULT = 0
+---Industry Changes
+	NDefines.NBuildings.MAX_SHARED_SLOTS = 50
+	MAX_BUILDING_LEVELS = 50
+	
+	NDefines.NBuildings.NAVALBASE_REPAIR_MULT = 0.2	
+---Graphics Changes
+	NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0;
+	NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0;
+---Passive Gameplay Changes
+	NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30
---
+++ 2670574659/descriptor.mod
@@ -0,0 +1,8 @@
+version="2.3.7"
+tags={
+	"Gameplay"
+}
+name="Vanilla++"
+picture="thumbnail.png"
+supported_version="1.11.*"
+remote_file_id="2670574659"
